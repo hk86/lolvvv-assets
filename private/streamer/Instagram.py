51e9827974acb855dacfe700739f9969ce629bc6
hpauli <hannes@lolvvv.com>
2018-03-30 23:01:51 +0200

    * self._api call fixed * proId fixed

--- private/streamer/Instagram.py
+++ private/streamer/Instagram.py
@@ -19,9 +19,9 @@ class Instagram:
         champ_hashes = ''
 
         for pro in live_match.getPros():
-            pro_hashes += '#' + self._db.getProKey(proId) + ' '
+            pro_hashes += '#' + self._db.getProKey(pro['pro']['proId']) + ' '
             champ_hashes += '#' + self._db.getChampionKey(pro['championId']) + ' '
 
         champ_hashes = champ_hashes[:-1] # remove space
         caption = title + '\n\nStream-Link in Bio!\n\n\n' + std_hashes + pro_hashes + champ_hashes
-        InstagramAPI.uploadPhoto(photo_path, caption=caption)
+        self._api.uploadPhoto(photo_path, caption=caption)
