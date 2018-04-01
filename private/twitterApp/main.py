c6458b11e3696b5ef3f7e44cca6eb46655838f9f
hp-git <lasthero88@gmail.com>
2018-04-01 21:15:28 +0200

    * app generates an tweet for the main twitter account

---
+++ private/twitterApp/main.py
@@ -0,0 +1,15 @@
+﻿import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'streamer'))
+
+from Database import Database
+from Logger import Logger
+from Twitter import Twitter
+
+if __name__ == "__main__":
+    db = Database('mongodb://root:ZTgh67gth1@10.8.0.6:27017/meteor?authSource=admin')
+    logger = Logger('twitterApp')
+
+    twitter = Twitter(db, logger)
+    twitter.mainTweet()
+
+    logger.info('finished')
