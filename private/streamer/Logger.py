617370065ec0f85837543569cf0ae6560392970f
hp-git <lasthero88@gmail.com>
2018-04-02 14:25:59 +0200

    * extracting logging functionality in own class

---
+++ private/streamer/Logger.py
@@ -0,0 +1,26 @@
+﻿import logging
+import sys
+
+class Logger:
+
+    def __new__(self, appName):
+        self = logging.getLogger(appName)
+        self.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+
+        fh = logging.FileHandler(appName + '.log')
+        fh.setLevel(logging.WARNING)
+        fh.setFormatter(formatter)
+        self.addHandler(fh)
+
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+        self.addHandler(ch)
+        return self
+
+    def __exit__(self):
+        for handler in self.handlers:
+            handler.close()
+            self.removeFilter(handler)
+        
