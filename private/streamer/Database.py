a1c5dc6897ee7d596e2a4efa271d0a68cab0c62a
hp-git <lasthero88@gmail.com>
2018-04-02 14:31:23 +0200

    * file lolvvv_scoreboard_insta.jpg set to ignore * Database ** _server_state saved as private reference ** method getMainTweetId, updateMainTweet, _createLast24hCriteria,    _getMostPlayedChampsOnStream, _getMostShownProsOnStream,    getMostPlayedChampsLast24Hours and getMostShownProLast24Hours implemented

--- private/streamer/Database.py
+++ private/streamer/Database.py
@@ -11,6 +11,7 @@ class Database:
         self.db = mc['meteor']
         self.active_matches = self.db['fact_active_matches']
         self.streamed_matches = self.db['fact_streamed_matches']
+        self._server_state = self.db['dim_server_state']
 
         self._cachePro = None
         self._cacheChamp = None
@@ -41,8 +42,7 @@ class Database:
             'streamGameEnding':0,
             'scannedForEvents':False}
         self.streamed_matches.insert_one(match)
-        server_state = self.db['dim_server_state']
-        server_state.update_one({}, {'$set': {'streaming': {'platformId': platformId, 'gameId': gameId} }})
+        self._server_state.update_one({}, {'$set': {'streaming': {'platformId': platformId, 'gameId': gameId} }})
 
     def setStreamingMatchEnd(self, gameId, platformId):
        self.streamed_matches.update_one({'$and': [{'gameId': gameId},
@@ -105,3 +105,67 @@ class Database:
         for pro in twitterPros:
             twitterIds.append(self._extractTwitterId(pro))
         return twitterIds
+
+    def getMainTweetId(self):
+        return self._server_state.find_one()['twitter']['live']['tweetId']
+
+    def updateMainTweet(self, tweetId):
+        self._server_state.update_one({}, {'$set': {{'twitter.live': {'tweetId': tweetId, 'tweetPostedTimestamp': int(time.time())} }} },upsert=False)
+
+    def _createLast24hCriteria(self):
+        streamed_matches_24h = self.db['fact_streamed_matches'].find(
+            {'streamGameEnding': { '$gt': int(time.time()) - 24*60*60 } },
+            { '_id': 0, 'platformId': 1, 'gameId': 1 }
+        )
+        # Generate a match criteria for the selection from the fact_matches_pro col
+        return [match for match in streamed_matches_24h]
+
+    def _getMostPlayedChampsOnStream(self, match_criteria, limit=3):
+        stats = []
+        pipeline = [
+            {'$match': { '$or': match_criteria }},
+            {'$group': {
+                    '_id': '$participant.championId',
+                    'count': {'$sum': 1}
+                }},
+            {'$sort': {'count': -1}},
+            {'$limit': limit}
+        ]
+        for aggr in self.db['fact_matches_pro'].aggregate(pipeline):
+            champ = self.db['static_champions'].find_one({'language': 'en_US', 'id': aggr['_id']})
+            stats.append({
+                'championId': aggr['_id'],
+                'championKey': champ['key'],
+                'championName': champ['name'],
+                'count': aggr['count'],
+            })
+        return stats
+    
+    def _getMostShownProsOnStream(self, match_criteria, limit=3):
+        stats = []
+        pipeline = [
+            {'$match': { '$or': match_criteria }},
+            {'$group': {
+                    '_id': '$participantIdentity.pro.proId',
+                    'count': {'$sum': 1}
+                }},
+            {'$sort': {'count': -1}},
+            {'$limit': limit}
+        ]
+        for aggr in self.db['fact_matches_pro'].aggregate(pipeline):
+            pro = self.db['static_pros'].find_one({'proId': aggr['_id']})
+            stats.append({
+                'proId': aggr['_id'],
+                'proKey': pro['key'],
+                'proNickName': pro['nickName'],
+                'count': aggr['count'],
+            })
+        return stats
+
+    def getMostPlayedChampsLast24Hours(self, limit=3):
+        criteria = self._createLast24hCriteria()
+        return self._getMostPlayedChampsOnStream(criteria, limit)
+
+    def getMostShownProLast24Hours(self, limit=3):
+        criteria = self._createLast24hCriteria()
+        return self._getMostShownProsOnStream(criteria, limit)
