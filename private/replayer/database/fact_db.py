474231383206485180755cb461dabf56439d9699
hp-git <lasthero88@gmail.com>
2018-12-10 17:50:59 +0100

    * find_one modified to a simple and expression

--- private/replayer/database/fact_db.py
+++ private/replayer/database/fact_db.py
@@ -15,11 +15,8 @@ class FactDataDb(Database):
             (self._match_cached.game_id != game_id) or
             (self._match_cached.platform_id != platform_id)):
             fact_data = self._fact_matches.find_one(
-                OrderedDict([
-                    ('platformId', platform_id),
-                    ('gameId', game_id),
-                ])
-            )
+                {'$and': [{'gameId': game_id},
+                    {'platformId':platform_id}]})
             if fact_data:
                 self._match_cached = FactMatch(platform_id, game_id, fact_data)
             else:
