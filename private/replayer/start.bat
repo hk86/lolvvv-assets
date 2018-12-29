8bcd4e84ad9896594282c1f02fa8b7b1fbe1a46f
hp-git <lasthero88@gmail.com>
2018-12-29 12:54:54 +0100

    * batch files from replayer and streamer unified

--- private/replayer/start.bat
+++ private/replayer/start.bat
@@ -1,8 +1,9 @@
 @echo off
-:: currently not working!
-goto :eof
 SET mypath=%~dp0
 SET script_path=%mypath:~0,-1%
 cd /d %script_path%
 ../prepend_script.bat
-python main.py
+:: start node js replay server
+start app.bat
+:: start clipper
+main.bat
