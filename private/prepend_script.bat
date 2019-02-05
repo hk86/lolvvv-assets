aad0fff7c316c02ee42c36c87721a416b48a32c0
hp-git <lasthero88@gmail.com>
2019-02-05 17:44:55 +0100

    waiting for openvpn connection established to meteor db instead of google.com

--- private/prepend_script.bat
+++ private/prepend_script.bat
@@ -1,3 +1,3 @@
 :: Wait for Connection to the Internet established
-powershell.exe -noprofile -command "while((Test-Connection -ComputerName 'www.google.de' -Quiet) -eq $False) {Start-Sleep -s 10}"
+powershell.exe -noprofile -command "while((Test-Connection -ComputerName '10.8.0.2' -Quiet) -eq $False) {Start-Sleep -s 10}"
 git pull
