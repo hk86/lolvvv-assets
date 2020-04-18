c0c71e1e590b24661dc043b0e2590f07076daa70
hpauli <hannes@lolvvv.com>
2020-04-18 14:40:09 +0200

    test internet connection with new server address

--- private/prepend_script.bat
+++ private/prepend_script.bat
@@ -1,3 +1,3 @@
 :: Wait for Connection to the Internet established
-powershell.exe -noprofile -command "while((Test-Connection -ComputerName '10.8.0.2' -Quiet) -eq $False) {Start-Sleep -s 10}"
+powershell.exe -noprofile -command "while((Test-Connection -ComputerName '88.99.138.8' -Quiet) -eq $False) {Start-Sleep -s 10}"
 git pull
